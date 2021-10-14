4e947bb8a7f8cee04a4b250eb4fa1f01eea5d9e0
Deotama <deotama11@gmail.com>
2021-10-14 22:33:34 +0700

    Second Commit

--- app.js
+++ app.js
@@ -1,8 +1,39 @@
-import { Express } from "express";
+const express = require("express");
 
 const app = express();
 
 app.set("view engine", "ejs");
 app.get("/", function (req, res) {
-    res.render('index', {})
+  var today = new Date();
+  var currentDay = today.getDay();
+  var day = "";
+
+  switch (currentDay) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+    default:
+      console.log("It's shouldn't be an error");
+  }
+  return res.render("list", { kindOfDay: day });
 });
+
+app.listen(3000, () => console.log("Example app listening on port 3000!"));
