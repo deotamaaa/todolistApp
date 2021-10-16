2ae2fc03ab140bbcd15d50f95b7da85389ecea0f
Deotama <deotama11@gmail.com>
2021-10-16 10:54:37 +0700

    Completed To Do List App

--- app.js
+++ app.js
@@ -1,28 +1,40 @@
 const express = require("express");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
 var items = ["Eat Food", "Buy Food", "Cook Food"];
+let workItems = [];
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-app.get("/", function (req, res) {
-  var today = new Date();
-  var options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  };
+app.use(express.static("public"));
 
-  var day = today.toLocaleDateString("en-US", options);
-  return res.render("list", { kindOfDay: day, newListItems: items });
+app.get("/", function (req, res) {
+  let day = date.getDate();
+  return res.render("list", { listTitle: day, newListItems: items });
 });
 
 app.post("/", function (req, res) {
   var item = req.body.newItem;
 
-  items.push(item);
-  res.redirect("/");
+  if (req.body.list === "work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+
+app.post("/work", function (req, res) {
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
 });
 
 app.listen(3000, () => console.log("Example app listening on port 3000!"));
